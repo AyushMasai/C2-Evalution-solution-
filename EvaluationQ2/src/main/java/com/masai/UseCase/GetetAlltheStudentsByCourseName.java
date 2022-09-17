e260daf5a1c79d3e02be3e6bb42f3534bdf5fb30
AyushMasai <108318111+AyushMasai@users.noreply.github.com>
2022-09-17 18:51:08 +0530

    Add files via upload

---
+++ EvaluationQ2/src/main/java/com/masai/UseCase/GetetAlltheStudentsByCourseName.java
@@ -0,0 +1,29 @@
+package com.masai.UseCase;
+
+import com.masai.DAO.CourseDAO;
+import com.masai.DAO.CourseDAOImpl;
+import com.masai.Exceptions.CourseException;
+import com.masai.Model.Student;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class GetetAlltheStudentsByCourseName {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        CourseDAO dao = new CourseDAOImpl();
+        System.out.println("Enter Course name:");
+        String cname = sc.next();
+
+        try {
+            List<Student> sList =dao.getAlltheStudentsByCourseName(cname);
+            for(Student s: sList) {
+                System.out.println(s);
+            }
+        }catch(CourseException ce) {
+            System.out.println(ce.getMessage());
+        }finally {
+            sc.close();
+        }
+    }
+}
---
+++ EvaluationQ2/src/main/java/com/masai/UseCase/RegisterTeacher.java
@@ -0,0 +1,24 @@
+package com.masai.UseCase;
+
+import com.masai.DAO.CourseDAO;
+import com.masai.DAO.CourseDAOImpl;
+import com.masai.Model.Teacher;
+
+import java.util.Scanner;
+
+public class RegisterTeacher {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        CourseDAO dao = new CourseDAOImpl();
+        System.out.println("Enter the Teacher details");
+        Teacher teac = new Teacher();
+        System.out.println("Enter the Teacher Name");
+        teac.setTeacherName(sc.next());
+        System.out.println("Enter the Teacher  Years of Experience:");
+        teac.setNoYearOfExperince(sc.nextInt());
+        System.out.println("Enter the Teacher email: ");
+        teac.setEmail(sc.next());
+
+        dao.registerTeacher(teac);
+    }
+}
